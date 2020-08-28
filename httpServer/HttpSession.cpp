2b9829d05457d6fc4db46ce3b424eb319667a881
caijh23 <caijh23@gmail.com>
2020-08-29 00:07:16 +0800

    blabla

---
+++ httpServer/HttpSession.cpp
@@ -0,0 +1,239 @@
+// 200：请求被正常处理 
+// 204：请求被受理但没有资源可以返回 
+// 206：客户端只是请求资源的一部分，服务器只对请求的部分资源执行GET方法，相应报文中通过Content-Range指定范围的资源。
+
+// 301：永久性重定向 
+// 302：临时重定向 
+// 303：与302状态码有相似功能，只是它希望客户端在请求一个URI的时候，能通过GET方法重定向到另一个URI上 
+// 304：发送附带条件的请求时，条件不满足时返回，与重定向无关 
+// 307：临时重定向，与302类似，只是强制要求使用POST方法
+ 
+// 400：请求报文语法有误，服务器无法识别
+// 401：请求需要认证 
+// 403：请求的对应资源禁止被访问 
+// 404：服务器无法找到对应资源 
+
+// 500：服务器内部错误 
+// 503：服务器正忙
+
+#include "HttpSession.h"
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+HttpSession::HttpSession()
+    : praseresult_(false),
+    keepalive_(true)
+{
+
+}
+
+HttpSession::~HttpSession()
+{
+    
+}
+
+bool HttpSession::PraseHttpRequest(std::string &msg, HttpRequestContext &httprequestcontext)
+{
+    //std::cout << "HttpServer::HttpParser" << std::endl;
+	std::string crlf("\r\n"), crlfcrlf("\r\n\r\n");
+	size_t prev = 0, next = 0, pos_colon;
+	std::string key, value;
+
+    bool praseresult = false;
+	//prase http request line
+	if ((next = msg.find(crlf, prev)) != std::string::npos)
+	{
+		std::string first_line(msg.substr(prev, next - prev));
+		prev = next;
+		std::stringstream sstream(first_line);
+		sstream >> (httprequestcontext.method);
+		sstream >> (httprequestcontext.url);
+		sstream >> (httprequestcontext.version);
+	}
+	else
+	{
+        std::cout << "msg" << msg << std::endl;
+		std::cout << "Error in httpPraser: http_request_line isn't complete!" << std::endl;
+        praseresult = false;
+        msg.clear();
+        return praseresult;
+	}
+    //prase http request header
+	size_t pos_crlfcrlf = 0;
+	if (( pos_crlfcrlf = msg.find(crlfcrlf, prev)) != std::string::npos)
+	{
+		while (prev != pos_crlfcrlf)
+        {
+            next = msg.find(crlf, prev + 2);
+            pos_colon = msg.find(":", prev + 2);
+            key = msg.substr(prev + 2, pos_colon - prev-2);
+            value = msg.substr(pos_colon + 2, next-pos_colon-2);
+            prev = next;
+            httprequestcontext.header.insert(std::pair<std::string, std::string>(key, value));
+        }
+	}
+    else
+    {
+        std::cout << "Error in httpPraser: http_request_header isn't complete!" << std::endl;
+        praseresult = false;
+        msg.clear();
+        return praseresult;
+    }	
+    //prase http request body
+	httprequestcontext.body = msg.substr(pos_crlfcrlf + 4);
+    praseresult = true;
+    msg.clear();
+    return praseresult;
+}
+
+void HttpSession::HttpProcess(const HttpRequestContext &httprequestcontext, std::string &responsecontext)
+{
+    //std::cout << "HttpServer::HttpProcess" << std::endl;
+    std::string responsebody;
+    std::string errormsg;
+    std::string path;
+    std::string querystring;
+    
+    if("GET" == httprequestcontext.method)
+    {
+        ;
+    }
+    else if("POST" == httprequestcontext.method)
+    {
+        ;
+    }
+    else
+    {
+        std::cout << "HttpServer::HttpParser" << std::endl;
+        errormsg = "Method Not Implemented";
+        HttpError(501, "Method Not Implemented", httprequestcontext, responsecontext);
+        return;
+    }
+
+    size_t pos = httprequestcontext.url.find("?");
+    if(pos != std::string::npos)
+    {
+        path = httprequestcontext.url.substr(0, pos);
+        querystring = httprequestcontext.url.substr(pos+1);
+    }
+    else
+    {
+        path = httprequestcontext.url;
+    }
+    
+    //keepalive判断处理
+    std::map<std::string, std::string>::const_iterator iter = httprequestcontext.header.find("Connection");
+    if(iter != httprequestcontext.header.end())
+    {
+        keepalive_ = (iter->second == "Keep-Alive");
+    }
+    else
+    {
+        if(httprequestcontext.version == "HTTP/1.1")
+        {
+            keepalive_ = true;//HTTP/1.1默认长连接
+        }
+        else
+        {
+            keepalive_ = false;//HTTP/1.0默认短连接
+        }            
+    }
+
+    
+    if("/" == path)
+    {        
+        path = "/index.html";
+    }
+    else if("/hello" == path)
+    {
+        std::string filetype("text/html");
+        responsebody = ("hello world");
+        responsecontext += httprequestcontext.version + " 200 OK\r\n";
+        responsecontext += "Server: My NetServer\r\n";
+        responsecontext += "Content-Type: " + filetype + "; charset=utf-8\r\n";
+        if(iter != httprequestcontext.header.end())
+        {
+            responsecontext += "Connection: " + iter->second + "\r\n";
+        }
+        responsecontext += "Content-Length: " + std::to_string(responsebody.size()) + "\r\n";
+        responsecontext += "\r\n";
+        responsecontext += responsebody;
+        return;
+    }
+    else
+    {
+        ;
+    }    
+
+    //std::string responsebody;    
+    path.insert(0,".");
+    FILE* fp = NULL;
+    if((fp = fopen(path.c_str(), "rb")) == NULL)
+    {
+        //perror("error fopen");
+        //404 NOT FOUND
+        HttpError(404, "Not Found", httprequestcontext, responsecontext);
+        return;
+    }
+    else
+    {
+        char buffer[4096];
+        memset(buffer, 0, sizeof(buffer));
+        while(fread(buffer, sizeof(buffer), 1, fp) == 1)
+        {
+            responsebody.append(buffer);
+            memset(buffer, 0, sizeof(buffer));
+        }
+        if(feof(fp))
+        {
+            responsebody.append(buffer);
+        }        
+        else
+        {
+            std::cout << "error fread" << std::endl;
+        }        	
+        fclose(fp);
+    }
+
+    std::string filetype("text/html"); //暂时固定为html
+    responsecontext += httprequestcontext.version + " 200 OK\r\n";
+    responsecontext += "Server: My HttpServer/0.1\r\n";
+    responsecontext += "Content-Type: " + filetype + "; charset=utf-8\r\n";
+    if(iter != httprequestcontext.header.end())
+    {
+        responsecontext += "Connection: " + iter->second + "\r\n";
+    }
+    responsecontext += "Content-Length: " + std::to_string(responsebody.size()) + "\r\n";
+    responsecontext += "\r\n";
+    responsecontext += responsebody;    
+}
+
+void HttpSession::HttpError(const int err_num, const std::string short_msg, const HttpRequestContext &httprequestcontext, std::string &responsecontext)
+{
+    std::string responsebody;
+    responsebody += "<html><title>出错了</title>";
+    responsebody += "<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"></head>";
+    responsebody += "<style>body{background-color:#f;font-size:14px;}h1{font-size:60px;color:#eeetext-align:center;padding-top:30px;font-weight:normal;}</style>";
+    responsebody += "<body bgcolor=\"ffffff\"><h1>";
+    responsebody += std::to_string(err_num) + " " + short_msg;
+    responsebody += "</h1><hr><em> My HttpServer</em>\n</body></html>";
+
+    std::string httpversion;
+    if(httprequestcontext.version.empty())
+    {
+        httpversion = "HTTP/1.1";
+    }
+    else
+    {
+        httpversion = httprequestcontext.version;
+    }   
+        
+    responsecontext += httpversion + " " + std::to_string(err_num) + " " + short_msg + "\r\n";
+    responsecontext += "Server: My HttpServer/0.1\r\n";
+    responsecontext += "Content-Type: text/html\r\n";
+    responsecontext += "Connection: Keep-Alive\r\n";
+    responsecontext += "Content-Length: " + std::to_string(responsebody.size()) + "\r\n";
+    responsecontext += "\r\n";
+    responsecontext += responsebody;
+}
---
+++ httpServer/Poller.cpp
@@ -0,0 +1,131 @@
+//
+//
+        //    typedef union epoll_data {
+        //        void    *ptr;
+        //        int      fd;
+        //        uint32_t u32;
+        //        uint64_t u64;
+        //    } epoll_data_t;
+
+        //    struct epoll_event {
+        //        uint32_t     events;    /* Epoll events */
+        //        epoll_data_t data;      /* User data variable */
+        //    };
+
+#include "Poller.h"
+#include <iostream>
+#include <stdio.h> //perror
+#include <stdlib.h> //exit
+#include <unistd.h> //close
+#include <errno.h>
+
+#define EVENTNUM 4096 //最大触发事件数量
+#define TIMEOUT 1000 //epoll_wait 超时时间设置
+
+Poller::Poller()
+    : pollfd_(-1),
+    eventlist_(EVENTNUM),
+    channelmap_(),
+    mutex_()
+{
+    pollfd_ = epoll_create(256);
+    if(pollfd_ == -1)
+    {
+        perror("epoll_create1");
+        exit(1);
+    }
+    std::cout << "epoll_create" << pollfd_ << std::endl;
+}
+
+Poller::~Poller()
+{
+    close(pollfd_);
+}
+
+void Poller::poll(ChannelList &activechannellist)
+{
+    int timeout = TIMEOUT;
+    int nfds = epoll_wait(pollfd_,&*eventlist_.begin(), (int)eventlist_.capacity(), timeout);
+    if (nfds == -1)
+    {
+        perror("epoll wait error");
+    }
+    for (int i = 0;i < nfds;i++)
+    {
+        int events = eventlist_[i].events;
+        Channel *pchannel = (Channel*)eventlist_[i].data.ptr;
+        int fd = pchannel->GetFd();
+
+        std::map<int, Channel*>::const_iterator iter;
+        {
+            std::lock_guard <std::mutex> lock(mutex_);
+            iter = channelmap_.find(fd);
+        }
+        if (iter != channelmap_.end())
+        {
+            pchannel->SetEvents(events);
+            activechannellist.push_back(pchannel);
+        }
+        else
+        {            
+            std::cout << "not find channel!" << std::endl;
+        }
+        if(nfds == (int)eventlist_.capacity())
+        {
+            std::cout << "resize:" << nfds << std::endl;
+            eventlist_.resize(nfds * 2);
+        }
+    }
+}
+
+//添加事件
+void Poller::AddChannel(Channel *pchannel)
+{
+    int fd = pchannel->GetFd();
+    struct epoll_event ev;
+    ev.events = pchannel->GetEvents();
+    ev.data.ptr = pchannel;
+    {
+        std::lock_guard <std::mutex> lock(mutex_);
+        channelmap_[fd] = pchannel;
+    }
+    if(epoll_ctl(pollfd_, EPOLL_CTL_ADD, fd, &ev) == -1)
+    {
+        perror("epoll add error");
+        exit(1);
+    }
+}
+
+//删除事件
+void Poller::RemoveChannel(Channel *pchannel)
+{
+    int fd = pchannel->GetFd();
+    struct epoll_event ev;
+    ev.events = pchannel->GetEvents();
+    ev.data.ptr = pchannel;
+    {
+        std::lock_guard <std::mutex> lock(mutex_);
+        channelmap_.erase(fd);
+    }    
+
+    if(epoll_ctl(pollfd_, EPOLL_CTL_DEL, fd, &ev) == -1)
+    {
+        perror("epoll del error");
+        exit(1);
+    }
+}
+
+//更新事件
+void Poller::UpdateChannel(Channel *pchannel)
+{
+    int fd = pchannel->GetFd();
+    struct epoll_event ev;
+    ev.events = pchannel->GetEvents();
+    ev.data.ptr = pchannel;
+
+    if(epoll_ctl(pollfd_, EPOLL_CTL_MOD, fd, &ev) == -1)
+    {
+        perror("epoll update error");
+        exit(1);
+    }
+}
